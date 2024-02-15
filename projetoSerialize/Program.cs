4004cf9ab38308cf74adb90da680be07db3af131
Edivaldo Leitão <mandoritos.ea@gmail.com>
2024-02-14 22:31:41 -0300

    criacao do json

--- projetoSerialize/Program.cs
+++ projetoSerialize/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+using projetoSerialize.Models;
+
+List<Venda> listaVendas = new List<Venda>();
+
+Venda venda = new Venda(1,"xampu", 12.00M);
+Venda venda2 = new Venda(2,"xampu 2", 11.00M);
+
+listaVendas.Add(venda);
+listaVendas.Add(venda2);
+
+//string serialized = JsonConvert.SerializeObject(venda, Formatting.Indented);
+string serialized = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+
+File.WriteAllText("Arquivos/vendas.json",serialized);
+
+Console.WriteLine(serialized);
